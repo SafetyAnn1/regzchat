3a46d9c65156478fdffa7e3e4bb70500420277ee
Ann Meier <annmeier3@gmail.com>
2019-11-05 01:01:53 +0000

    Heroku Update to Redis

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
   has_many :room_messages, dependent: :destroy,
                          inverse_of: :room
+  has_many :users, through: :room_messages
+  validates :topic, presence: true, uniqueness: true, case_sensitive: false
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-   validates :username, uniqueness: true, presence: true
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :room_messages
+  has_many :rooms, through: :room_messages
+  validates :username, uniqueness: true, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    def gravatar_url
+  def gravatar_url
     gravatar_id = Digest::MD5::hexdigest(email).downcase
     "https://gravatar.com/avatar/#{gravatar_id}.png"
-    end
+  end
 end
