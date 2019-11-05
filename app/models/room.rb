505252c063ce2e1b4719653c9cd1d92c31851e1a
Ann Meier <annmeier3@gmail.com>
2019-11-05 01:33:50 +0000

    Fixed Room Creation Dev and Prod

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,5 +2,4 @@ class Room < ApplicationRecord
   has_many :room_messages, dependent: :destroy,
                          inverse_of: :room
   has_many :users, through: :room_messages
-  validates :topic, presence: true, uniqueness: true, case_sensitive: false
 end
